d0259101cd699fb2330529b7a2a6610da7cff304
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-01-13 21:47:53 -0800

    second attempt at MVP

--- index.js
+++ index.js
@@ -245,12 +245,12 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(//array) {
+function get20s(array) {
   //const newArray = []
   //for(let i = 0; i < array.length; i++){
     //artists year >= 1900 && <= 2000
      
-  //}
+  }
 
 
 
@@ -329,16 +329,11 @@ Use artistInfo to do the following:
 For example artistInfo(artists, 'Frida Kahlo') will return: 
   ["Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."]
 */
-function artistInfo(array){
-  const bio = [];
-  for(let i = 0; i < artists.length; i++){
-    if(artists[i]['bio'] === bio){
-      number.push(artists[i]['bio']);
-    }
-  }
-  return bio;
+function artistInfo(array, name){
+  //return array[name].bio
 }
-console.log(artistInfo(artists, 'Frida Kahlo'))
+//console.log(artistInfo(artists, 'Frida Kahlo'))
+
 
 
 
